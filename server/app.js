b523a363f4fd8e1aa7358d977bd6f74f8b92757e
Artur-oa <arturogan@yandex.ru>
2025-06-26 20:07:06 +0300

    create server

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serverConfig = require('./src/configs/serverConfig');
+const indexRouter = require('./src/routes/indexRouter');
+
+const app = express();
+
+serverConfig(app);
+
+app.use('/api', indexRouter);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
