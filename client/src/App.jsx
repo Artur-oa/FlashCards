42e4a0f02ff3824079430aa69afcfafba70a269e
alx-almkn <alimkin1990@gmail.com>
2025-06-27 13:24:16 +0300

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Layout from "./app/Layout";
 import UserForm from "./pages/UserForm/userForm";
 import GamePage from "./pages/GamePage";
 import MainPages from "./pages/MainPages/MainPages";
+import DecksPage from "./pages/DeckPage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+        <Route path='/deck'element={<DecksPage />} />
           <Route path="/" element={<MainPages />} />
           <Route path="/users" element={<UserForm />} />
           <Route path="/game/:deckId" element={<GamePage />} />
