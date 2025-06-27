ec08c1ae6cc540980c10ffa4a05f9fe23a7646ed
Gans-Shtraus <pugachev.2013@yandex.ru>
2025-06-27 12:32:19 +0300

    release css and fix userForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import Layout from "./app/Layout";
 import UserForm from "./pages/UserForm/userForm";
+import MainPages from "./pages/MainPages/MainPages";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="/" element={<MainPages />} />
           <Route path="/users" element={<UserForm />} />
         </Route>
       </Routes>
