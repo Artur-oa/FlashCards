bec92019f68bf54f13ed1d9513815de701600634
alx-almkn <alimkin1990@gmail.com>
2025-06-27 12:19:33 +0300

    createPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router";
 import Layout from "./app/Layout";
 import UserForm from "./pages/UserForm/userForm";
-
+import GamePage from "./pages/GamePage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/users" element={<UserForm />} />
+          <Route path="/game/:deckId" element={<GamePage />} />
         </Route>
       </Routes>
     </>
