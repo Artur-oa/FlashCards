9b29f6f73f0d5d808483b71ff116340fcb5efcc1
alx-almkn <alimkin1990@gmail.com>
2025-06-27 09:50:01 +0300

    crateCardService

--- server/src/services/Card.service.js
+++ server/src/services/Card.service.js
@@ -4,17 +4,21 @@ const CardService = {
   async getAll() {
     return Card.findAll();
   },
+  
   async getOne(id) {
     return Card.findByPk(id);
   },
+
   async create(data) {
     return Card.create(data);
   },
+
   async update(id, data) {
     const card = await Card.findByPk(id);
     if (!card) return null;
     return card.update(data);
   },
+
   async delete(id) {
     const card = await Card.findByPk(id);
     if (!card) return null;
